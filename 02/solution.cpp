aabc9d6e639c7c08d824f65524e64e0dba2b3d3e
Matt L <primeMuon@gmail.com>
2020-12-02 17:14:22 +0000

    Add 2020 day 2 solutions

---
+++ 02/solution.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define SIZE 1000
+
+using namespace std;
+
+bool isValidQ1(int lower, int upper, char target, string password)
+{
+	// count the number of give character
+	int valid = 0;
+	for(char ch : password)
+	{
+		if(ch == target)
+			valid++;
+	}
+
+	return (lower <= valid) && (valid <= upper);
+}
+
+bool isValidQ2(int lower, int upper, char target, string password)
+{
+	int count = 0;
+
+	count += (password.at(lower - 1) == target)? 1 : 0;
+	count += (password.at(upper - 1) == target)? 1 : 0;
+
+	return count == 1;
+}
+
+void countValidSets()
+{
+	ifstream file;
+	file.open("./data.txt");
+
+	int count = 0;
+
+	int lower, upper;
+	char ch, rubbish;
+	string password;
+
+	for(int i=0; i<SIZE; i++)
+	{
+		// read one line from the file
+		file >> lower >> rubbish >> upper >> ch >> rubbish >> password;
+
+		// count for Q1
+		// count += isValidQ1(lower, upper, ch, password)? 1 : 0;
+
+		// count for Q2
+		count += isValidQ2(lower, upper, ch, password)? 1 : 0;		
+	}
+
+	cout << count << endl;
+
+	file.close();
+}
+
+int main()
+{
+	countValidSets();
+
+	return 0;
+}

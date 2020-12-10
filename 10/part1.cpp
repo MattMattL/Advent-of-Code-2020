45fb162adc7c2a1c7809e594d4984d76a7edc3bb
Matt L <primeMuon@gmail.com>
2020-12-10 21:21:52 +0000

    Refactor file name

---
+++ 10/part1.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <fstream>
+#include "../dataHelper.hpp"
+
+using namespace std;
+
+Data<int> data;
+
+void countDifferences()
+{
+	int diff_1 = 0, diff_3 = 0;
+
+	data.add_front(0);
+	data.add_back(data.at(data.size() - 1) + 3);
+
+	for(int i=0; i<data.size() - 1; i++)
+	{
+		switch(data.at(i + 1) - data.at(i))
+		{
+			case 1:
+				diff_1++; break;
+			case 3:
+				diff_3++; break;
+		}
+	}
+
+	// Part 1 answer
+	cout << diff_1 * diff_3 << endl;
+}
+
+int main()
+{
+	data.read("./data.txt");
+	data.sort();
+
+	countDifferences();
+
+	return 0;
+}
